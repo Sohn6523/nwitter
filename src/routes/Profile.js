e6b2e534c64cac6d6207345322a1043f40b16c70
swsohn <swsohn@swsohnui-MacBookPro.local>
2020-10-21 00:12:51 +0900

    Profile Update

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,42 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useState } from "react";
+import { authService, dbService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-export default () => {
-    const onLogOutClick = () => authService.signOut();
-    return <>
-        <button onClick={onLogOutClick}>Log out</button>
-    </>;
-}
+export default ({ refreshUser, userObj }) => {
+    const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.pushState("/");
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            });
+            refreshUser();
+        }
+    };
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Display name"
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
